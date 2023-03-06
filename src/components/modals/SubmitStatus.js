fc69c256bc607036330fe5b559ed8016194655b0
Ujjwal Sharma <Ujjwal.Sharma@dhiway.com>
2023-03-06 20:01:46 +0530

    success close button

--- src/components/modals/SubmitStatus.js
+++ src/components/modals/SubmitStatus.js
@@ -4,7 +4,8 @@ const SubmitStatus = (props) => {
 
   return (
     <div className='fixed flex z-50 top-0 left-0 right-0 bottom-0 justify-center items-center w-full h-full bg-modal-black'>
-        <div className='w-max px-20 h-1/3 flex flex-col gap-4 justify-center items-center min-h-min bg-white rounded-md'>
+        <div className='relative w-max px-20 h-1/3 flex flex-col gap-4 justify-center items-center min-h-min bg-white rounded-md'>
+            <span onClick={props.close} className='cursor-pointer absolute right-4 top-4 text-lg'>X</span>
             <h6 className='text-2xl text-design-purple'>Congratulations! {props.name}</h6>
             <p className='text-base p-0'>Your Resume has been succesfully uploaded.</p>
             <button onClick={props.close}
